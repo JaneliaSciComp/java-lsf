384918fc2a7828fc7bd900d213455e77da88671c
nat-alzofon <nathaniel.alzofon@cuanschutz.edu>
2022-09-06 01:11:19 -0600

    Added ability to terminate the job monitor thread once stop is called.

--- src/main/java/org/janelia/cluster/JobMonitor.java
+++ src/main/java/org/janelia/cluster/JobMonitor.java
@@ -19,9 +19,11 @@ public class JobMonitor {
 
     // Constants
     private static final int DEFAULT_CHECK_INTERVAL_SECONDS = 15;
+    private static final boolean DEFAULT_KEEP_THREAD_ALIVE = true;
 
     // Configuration
     private final int checkIntervalSeconds;
+    private final boolean keepThreadAlive;
 
     // State
     private JobManager jobManager;
@@ -31,12 +33,21 @@ public class JobMonitor {
 
 
     public JobMonitor(JobManager jobManager) {
-        this(jobManager, DEFAULT_CHECK_INTERVAL_SECONDS);
+        this(jobManager, DEFAULT_CHECK_INTERVAL_SECONDS, DEFAULT_KEEP_THREAD_ALIVE);
     }
 
     public JobMonitor(JobManager jobManager, int checkIntervalSeconds) {
+        this(jobManager, checkIntervalSeconds, DEFAULT_KEEP_THREAD_ALIVE);
+    }
+
+    public JobMonitor(JobManager jobManager, boolean keepThreadAlive) {
+        this(jobManager, DEFAULT_CHECK_INTERVAL_SECONDS, keepThreadAlive);
+    }
+
+    public JobMonitor(JobManager jobManager, int checkIntervalSeconds, boolean keepThreadAlive) {
         this.jobManager = jobManager;
         this.checkIntervalSeconds = checkIntervalSeconds;
+        this.keepThreadAlive = keepThreadAlive;
     }
 
     /**
@@ -55,14 +66,17 @@ public class JobMonitor {
     }
 
     /**
-     * Stop monitoring the cluster. After calling stop(), the client may call start() to begin monitoring again.
-     * If the monitor is not running, calling this method does nothing.
+     * Stop monitoring the cluster. If keepThreadAlive==false: After calling stop(), the client may call start() to begin monitoring again.
+     * If the monitor is not running, calling this method does nothing. If keepThreadAlive==true: After calling stop(), the client cannot call start() again, because the thread will die.
      */
     public synchronized void stop() {
         if (started) {
             log.debug("Stopping job monitoring");
             jobChecker.cancel(true);
             started = false;
+            if (!this.keepThreadAlive) {
+                this.scheduler.shutdown();
+            }
         }
     }
 }
